89e319a24aa5ef843a542d6382d26facb4cdb261
theproz99 <bmitsevich@gmail.com>
2020-10-28 19:58:54 -0500

    Basic arrow key movement done
    See above

--- Assets/Scripts/BigPumpkin.cs
+++ Assets/Scripts/BigPumpkin.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class BigPumpkin : MonoBehaviour
 {
+    public int starthp;
+    int hp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = starthp;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bullet")
+        {
+            hp -= 1;
+            print(hp);
+        }
         
     }
 }
--- Assets/Scripts/BigPumpkinMovement.cs
+++ Assets/Scripts/BigPumpkinMovement.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BigPumpkinMovement : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
+    /*
     void Start()
     {
         
     }
-
+    */
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
     }
 }
