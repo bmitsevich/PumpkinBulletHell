c8a99780535fcbbe67bb03f87d6a4665f2fe9aa4
theproz99 <bmitsevich@gmail.com>
2020-10-28 20:34:24 -0500

    Functional smallp, bigp needs some work

--- Assets/Scripts/BigPumpkin/BigPumpkinMovement.cs
+++ Assets/Scripts/BigPumpkin/BigPumpkinMovement.cs
@@ -15,6 +15,6 @@ public class BigPumpkinMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
     }
 }
---
+++ Assets/Scripts/SmallPumpkin/SmallPumpkinMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallPumpkinMovement : MonoBehaviour
+{
+    Transform rotationCenter;
+
+    public float rotationRadius;
+    public float angularSpeed;
+
+    public float posX, posY, angle;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+            posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+            transform.position = new Vector2(posX, posY);
+            angle = angle + Time.deltaTime * angularSpeed;
+            if (angle >= 360f)
+            {
+                angle = 0f;
+            }
+        }
+    }
+}
