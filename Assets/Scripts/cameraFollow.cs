9ae9817f6342ae2c39e7a612eb9c1936bb891ad0
Andrew Tran <andtran0703@gmail.com>
2020-10-30 01:15:10 -0400

    upload crosshair

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class cameraFollow : MonoBehaviour {
 
-    public Transform bigPumpkin, minVertWall, maxVertWall, minHorizWall, maxHorizWall;
+    public Transform bigPumpkin, leftWall, rightWall, bottomWall, topWall;
     public float cameraDistance = 30.0f;
 
     void Awake ()
@@ -14,8 +14,8 @@ public class cameraFollow : MonoBehaviour {
 
     void Update ()
     {
-        transform.position = new Vector3(Mathf.Clamp(bigPumpkin.position.x, minHorizWall.position.x, maxHorizWall.position.x),
-                                        (Mathf.Clamp(bigPumpkin.position.y, minVertWall.position.y, maxVertWall.position.y)),
+        transform.position = new Vector3(Mathf.Clamp(bigPumpkin.position.x, -11, 11),
+                                        (Mathf.Clamp(bigPumpkin.position.y, bottomWall.position.y, topWall.position.y)),
                                         (transform.position.z)
                                         );
     }
