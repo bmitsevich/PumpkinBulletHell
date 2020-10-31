0fcb5fd8bef426d5d168fab3e01acdce448af79e
Andrew Tran <andtran0703@gmail.com>
2020-10-31 00:20:01 -0400

    soundEffects scripts push

---
+++ Assets/Scripts/SFXPlaying.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlaying : MonoBehaviour
+{
+    public AudioSource playerShot;
+    //public AudioSource enemyShot;
+    public AudioSource playerDeath;
+    //public AudioSource enemyDeath;
+
+    float timer, cooldown = .3f;
+    public void playPlayerShot() {
+        playerShot.Play ();
+    }
+    //public void playEnemyShot() {
+    //    enemyShot.Play ();
+    //}
+    public void playPlayerDeath() {
+        playerDeath.Play ();
+    }
+    //public void playEnemyDeath() {
+    //    enemyDeath.Play ();
+    //}
+    void Update() {
+        if(Input.GetKey("space") && timer <= 0) {
+            playPlayerShot();
+            timer = cooldown;
+        }
+        timer -= Time.deltaTime;
+        if(GameObject.FindGameObjectsWithTag("Player").Length == 1) {
+            playPlayerDeath();
+        }
+    }
+
+}
