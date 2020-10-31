6849ede8f35861c7b49b67f9c5136bbeb04d6f60
theproz99 <bmitsevich@gmail.com>
2020-10-30 21:34:17 -0500

    spawnneridk

--- Assets/Scripts/Spawner/MonstersSpawnerControl.cs
+++ Assets/Scripts/Spawner/MonstersSpawnerControl.cs
@@ -7,22 +7,103 @@ public class MonstersSpawnerControl : MonoBehaviour {
 	public Transform[] spawnPoints;
 	public GameObject[] monsters;
 	int randomSpawnPoint, randomMonster;
-	public static bool spawnAllowed;
+	public static bool spawnAllowed = true;
+	public static int wave;
+	public static int numMonsters;
+	public int curMonsters;
+	public float waveTimer = 5.0f;
+	public float timeTillWave = 0.0f;
+
+	public float timeBeforeSpawning = 1.5f;
+	public float timeBetweenEnemeis = 1.0f;
+
+
+	private IEnumerator coroutine;
+	private IEnumerator WaitAndPrint(float waitTime)
+    {
+		yield return new WaitForSeconds(waitTime);
+		SpawnAMonster();
+    }
+	
 
 	// Use this for initialization
 	void Start () {
+		wave = 1;
+		numMonsters = 5;
+		curMonsters = 0;
 		spawnAllowed = true;
-		InvokeRepeating ("SpawnAMonster", 0f, 1f);
+		StartCoroutine(SpawnMonsters());
+		//InvokeRepeating("SpawnAMonster", 0f, 1f);
+		
 	}
 
-	void SpawnAMonster()
+     void Update()
+    {
+		/*
+        if (spawnAllowed)
+        {
+			curMonsters++;
+			Invoke("SpawnAMonster", 1.0f);
+			//coroutine = WaitAndPrint(3.0f);
+			//StartCoroutine(coroutine);
+        }
+
+		if (curMonsters > numMonsters)
+        {
+			spawnAllowed = false;
+	
+        }
+		
+		if (!spawnAllowed) //time until next wave
+        {
+			timeTillWave += Time.deltaTime;
+			if (timeTillWave >= waveTimer)
+            {
+				print("SUP");
+				spawnAllowed = true;
+				timeTillWave = 0.0f;
+				curMonsters = 0;
+
+			}
+        }
+		*/
+		
+    }
+    void SpawnAMonster()
 	{
-		if (spawnAllowed) {
-			randomSpawnPoint = Random.Range (0, spawnPoints.Length);
-			randomMonster = Random.Range (0, monsters.Length);
-			Instantiate (monsters [randomMonster], spawnPoints [randomSpawnPoint].position,
-				Quaternion.identity);
-		}
+		
+		randomSpawnPoint = Random.Range (0, spawnPoints.Length);
+		randomMonster = Random.Range (0, monsters.Length);
+		Instantiate (monsters [randomMonster], spawnPoints [randomSpawnPoint].position,
+		Quaternion.identity);
+		
+
 	}
+	
+	IEnumerator SpawnMonsters()
+    {
+		yield return new WaitForSeconds(timeBeforeSpawning);
+
+		while(true)
+        {
+			if (curMonsters <= 0)
+            {
+				for (int i = 0; i < numMonsters; i++)
+                {
+					SpawnAMonster();
+					print(curMonsters);
+					curMonsters++;
+					yield return new WaitForSeconds(timeBetweenEnemeis);
+                }
+            }
+			/*if (GameObject.FindGameObjectsWithTag("Hostile").Length <= 0)
+            {
+				yield return new WaitForSeconds(waveTimer);
+				curMonsters = 0;
+			}
+			*/
+			
+        }
+    }
 
 }
